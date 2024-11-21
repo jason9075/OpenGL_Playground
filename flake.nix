95f37302cbda45e311042b7d4c15cb03e7a1f765
jason9075 <jason9075@users.noreply.github.com>
2024-11-22 00:36:36 +0800

    update flake.nix

--- flake.nix
+++ flake.nix
@@ -2,12 +2,11 @@
   description = "flake.nix";
 
   inputs = {
-    # nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
     nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
     nixgl.url = "github:nix-community/nixGL";
   };
 
-  outputs = inputs@{ self, nixpkgs, nixgl, ... }:
+  outputs = { nixpkgs, nixgl, ... }:
     let
       system = "x86_64-linux";
       pkgs = import nixpkgs {
@@ -15,6 +14,21 @@
         overlays = [ nixgl.overlay ];
       };
     in {
-      devShells.x86_64-linux.default = import ./shell.nix { inherit pkgs; };
+      devShells.x86_64-linux.default = pkgs.mkShell {
+        nativeBuildInputs = with pkgs; [
+          gcc
+          cmake
+          SDL2
+          glew
+          glm
+          nlohmann_json
+          entr
+        ];
+
+        shellHook = ''
+          export LD_LIBRARY_PATH="/run/opengl-driver/lib:/run/opengl-driver-32/lib:$LD_LIBRARY_PATH"
+          echo "Nix env activated."
+        '';
+      };
     };
 }
